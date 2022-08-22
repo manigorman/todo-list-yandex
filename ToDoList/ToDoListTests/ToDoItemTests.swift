15d6d96cea3230c0216c2544c5a56d0b530bd7ee
manigorman <immanakov@gmail.com>
2022-08-22 16:41:50 +0300

    Resolved several comments

--- ToDoList/ToDoListTests/ToDoItemTests.swift
+++ ToDoList/ToDoListTests/ToDoItemTests.swift
@@ -10,14 +10,6 @@ import XCTest
 
 class ToDoItemTests: XCTestCase {
     
-//    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-    
-//    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-    
     func testToDoItem_canCreateInstance() {
         
         let partialInstance = ToDoItem(text: "Hello world",
